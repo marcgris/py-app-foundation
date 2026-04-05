400abd17c7cd21d96889c0c8305e0a1064a18430
marcgris <31602010+marcgris@users.noreply.github.com>
2026-04-05 12:45:04 -0500

    chore: format exception imports in package init

--- src/starter/__init__.py
+++ src/starter/__init__.py
@@ -4,12 +4,7 @@ __version__ = "0.4.0"
 __author__ = "Py App Foundation Contributors"
 
 from starter.config import Settings
-from starter.exceptions import (
-    AppRuntimeError,
-    ConfigError,
-    StarterError,
-    ValidationError,
-)
+from starter.exceptions import AppRuntimeError, ConfigError, StarterError, ValidationError
 from starter.logging import get_logger, setup_logging
 
 __all__ = [
